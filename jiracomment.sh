80656e3bbeeb22593ad1ae01900ee7fa76c4c62e
ihommani <ismael.hommani@fullsix.com>
2013-09-11 20:18:07 +0200

    First functional version

--- jiracomment.sh
+++ jiracomment.sh
@@ -1,24 +1,8 @@
 #!/bin/bash
 
 
-checkDependency(){
-command -v $1 >/dev/null 2>&1 || { echo >&2 "[ERROR] ${1} is required but is not installed.  Aborting."; exit 1;}
-}
-
-selectStatement(){
-local PS3="${1} ? --> "
-cd $3
-select answer in $($2)
-do
-    if [[ -n $answer ]]
-    then
-        cd -
-        break
-    else
-        echo "Invalid choice"
-    fi
-done
-}
+source ./core_functions.sh
+source ./ui_functions.sh
 
 echo "
        _ _____ _____              _          _                 _ 
@@ -33,67 +17,68 @@ echo "
 
 
 echo -ne "# \033[7mDEPENDENCY CHECK...\033[m "
-for dependency in git xclip mvn
-do
+for dependency in git xclip mvn; do
     checkDependency $dependency
 done
 
 if [ ! -f ~/.jiracomments ]; then
-	echo "config file ~/.jiracomments is missing. Aborting."
+	log ERROR "config file ~/.jiracomments is missing. Aborting."
 	exit 1
 fi 
 
 project_path=$(cat  ~/.jiracomments)
 if [ ! -d $project_path ]; then
-	echo "invalid config file: $project_path is not a directory. Aborting."
+	log ERROR "invalid config file: $project_path is not a directory. Aborting."
 	exit 1
 fi
 
 
 echo " OK!"
-echo "[INFO] Project root directory: $project_path"
+log INFO "Project root directory: $project_path"
 echo 
 
 echo -e "# \033[7mQUESTION TIME!\033[m"
 
 # project folder
-echo "Which maven project ?"
+
+echo "Which Maven project?"
+
 selectStatement Project ls $project_path
+
 project=$answer
 project_pom="$project_path/$project/pom.xml"
 project_git_location="$project_path/$project/.git"
 
 
-# Some basic verification. Fail early principe
-if [ ! -f  $project_pom ]
-then
-	echo "[ERROR] No POM file found in the project directory. Aborting."
+# Some basic verification. Fail-fast principle
+if [ ! -f  $project_pom ]; then
+	log ERROR "No POM file found in the project directory. Aborting."
     exit 1
 fi
 
 if [ ! -d $project_git_location ]
 then 
-    echo "[ERROR] The provided directory is not a git repository. Aborting."
+    log ERROR "The provided directory is not a git repository. Aborting."
     exit 1
 
 fi
+
 
 # version
 
 echo -n "## Grabbing Maven artifact version... "
 
-if [ -f "$project_pom" ]
-then 
+if [ -f "$project_pom" ]; then 
 	project_version=$(mvn help:evaluate -Dexpression=project.version -f $project_pom | grep '^[0-9].*')
 fi
 
 if [ -z "$project_version" ]; then
 	echo "no version found!"
-	echo "[ERROR] Either you don't follow a semantic versioning scheme or this is not a Maven project. Aborting."
+	log ERROR "Either you don't follow a semantic versioning scheme or this is not a Maven project. Aborting."
 	exit 1
 else
 	echo " OK!"
-	echo "[INFO] version: $project_version"
+	log INFO "version: $project_version"
 	echo ""
 fi
 
@@ -103,9 +88,9 @@ fi
 read -p "## Override default project hosted name (y/n)? (current value: $project) " -n 1 -r
 if [[ $REPLY =~ ^[Yy]$ ]]; then
     while [ -z "$project_hosted_name" ]; do
-		echo ""
-		echo -ne "\t... which is? "
-		read project_hosted_name
+	echo ""
+	echo -ne "\t... which is? "
+	read project_hosted_name
         echo ""
 	done
 else
@@ -114,51 +99,36 @@ fi
 
 
 # branch
-echo "Which branch concern the modification ?"
+
+echo "On which branch are the modifications?"
 echo $project_git_location
 selectStatement Branch "git branch" $project_git_location
 branch=$answer
 
 # commits
+
 author=$(git config user.name)
-echo -e "## Which commit \033[1mSHA1\033[m?"
-while [ ! -s /tmp/log_commit ] || [[ $REPLY =~ ^[Yy]$ ]]
-do
-    echo 'git --git-dir=${project_git_location} log -10 --author=${author} --format="%h %s"'
-    `git --git-dir=${project_git_location} log -10 --author=$author --format="%h %s"` > /tmp/log_commit
+echo -e "## Which commit \033[1mSHA1s\033[m?"
+
+while [[ -z ${short_hash} || $REPLY =~ ^[Yy]$ ]]; do
+    echo "# Select a commit by prepending 'x ' (without quotes)" > /tmp/log_commits
+    git --git-dir="${project_git_location}" log -10 --author="${author}" --format='%h %s' >> /tmp/log_commits
     $EDITOR /tmp/log_commits
-    sha1=$(grep '^x ' /tmp/log_commits)
-    echo $sha1
-	read -p "Do you want to add more commits(y/n)? " -n 1 -r
-	echo ""
+    messages="$(grep '^x ' /tmp/log_commits)"
+    for msg in "$messages"; do
+	    short_hash+=$(echo $msg | cut -d " " -f2)
+    done
+    echo "$short_hash"
+    read -p "Redo commit picking (y/n)? " -n 1 -r
+    echo ""
 done
-
-
-#while [ -z "$message" ] || [[ $REPLY =~ ^[Yy]$ ]]
-#do
-#	echo -e "## Which commit \033[1mSHA1\033[m?"
-##	echo -ne "\t"
-#	read sha1
-#	
-#	if [ -n "$sha1" ]
-#    then
-#		message=$(git --git-dir=$project_git_location log --format=%B $sha1 | head -n 1)
-#		if [ -n "$message" ]; then
-#			sha1s+=" "$sha1
-#			## FIXME: echo print \n instead of printing newline 
-#			messages+="\n"$message
-#		fi
-#	fi 
-#
-#	read -p "Other SHA1s (y/n)? " -n 1 -r
-#	echo ""
-#done
 
 
 
 # description override
 
 default_description=$messages
+echo "${default_description}" | cut -d " " -f3-
 read -p "## Override description (y/n/skip)? " -n 1 -r
 echo ""
 if [[ $REPLY =~ ^[Yy]$ ]]; then
@@ -171,7 +141,7 @@ elif [[ $REPLY =~ ^[sS]$ ]]
 then
 	description=''
 else
-	description=$default_description
+	description=$(echo ${default_description} | cut -d " " -f2)
 fi
 
 echo ""
@@ -181,18 +151,16 @@ h6. $project_hosted_name
  * Branch: $branch
  * Version: $project_version
  * Commit(s)"
-
-for sha1 in $sha1s
-do
-    ## Warning, this line is valid only if the provided SHA is in the current branch FIXME
-    complete_sha1=$(git --git-dir=$project_git_location log --format=raw | grep $sha1 | cut -d " " -f2)
-	## TODO: check structure
-	echo " ** http://gitlab.fullsix.net/$project_hosted_name/commits/$branch/$complete_sha1"
+short_hashes=$(echo "${default_description}" | cut -d " " -f2) > /dev/null 2>&1 
+for hash in $short_hashes; do
+    complete_sha1=$(git --git-dir="$project_git_location" log --format="%H" $hash -1)
+	echo " ** http://gitlab.fullsix.net/$project_hosted_name/commit/$branch/$complete_sha1"
 done
 
 if [ -n description ]
 then
-	echo -e " * Description: $description"
+	echo -e " * Description:"
+echo "${default_description}" | cut -d " " -f2-
 fi
 
 
