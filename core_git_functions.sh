d5dc3b4f936dab0b245396ac60de06a911e05568
Florent Biville <florent.biville@fullsix.net>
2013-09-17 17:49:52 +0200

    Fix ambiguity if branches start with same name

--- core_git_functions.sh
+++ core_git_functions.sh
@@ -8,5 +8,5 @@ jirac_get_git_project_root_directory() {
 
 jirac_get_git_current_remote_branch() {
 	local branch=$(git rev-parse --symbolic --abbrev-ref $(git symbolic-ref HEAD))
-    	echo $(git for-each-ref --format='%(refname:short):%(upstream:short)' refs/heads | grep $branch | cut -d: -f2)
+    	echo $(git for-each-ref --format='%(refname:short):%(upstream:short)' refs/heads | grep "$branch:" | cut -d: -f2)
 }
