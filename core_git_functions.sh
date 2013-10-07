a6cc0eeafdd6562af68b9c930bf16b2f51cebc85
Sebastien Lesaint <lesaint@ekino.com>
2013-10-07 15:02:45 +0200

    Fix JIRAc for Windows.

--- core_git_functions.sh
+++ core_git_functions.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 jirac_get_git_project_root_directory() {
-    	if [ -d .git ]; then
-		echo $(git rev-parse --show-toplevel)
-    	fi
+	if [ -d .git ]; then
+	echo $(git rev-parse --show-toplevel)
+	fi
 }
 
 jirac_get_git_current_remote_branch() {
 	local branch=$(git rev-parse --symbolic --abbrev-ref $(git symbolic-ref HEAD))
-    	echo $(git for-each-ref --format='%(refname:short):%(upstream:short)' refs/heads | grep "$branch:" | cut -d: -f2)
+	echo $(git for-each-ref --format='%(refname:short):%(upstream:short)' refs/heads | grep "$branch:" | cut -d: -f2)
 }
 
 jirac_get_git_full_message() {
--- core_xml_functions.sh
+++ core_xml_functions.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
+xml_parser() {
+	if [[ $OSTYPE == msys ]]; then
+		echo "xml"
+	else
+		echo "xmlstartlet"
+	fi
+}
+
 jirac_get_maven_version() {
-	echo $(xmlstarlet sel -N ns="http://maven.apache.org/POM/4.0.0" -t -m "/ns:project/ns:version/text()" -c . -n $1)
+	cmd=`xml_parser`
+	echo $($cmd sel -N ns="http://maven.apache.org/POM/4.0.0" -t -m "/ns:project/ns:version/text()" -c . -n "$1")
 }
 
 jirac_get_maven_project_name() {
-	echo $(xmlstarlet sel -N ns="http://maven.apache.org/POM/4.0.0" -t -m "/ns:project/ns:name/text()" -c . -n $1)
+	cmd=`xml_parser`
+	echo $($cmd sel -N ns="http://maven.apache.org/POM/4.0.0" -t -m "/ns:project/ns:name/text()" -c . -n "$1")
 }
 
 jirac_get_scm_url() {
-	echo $(xmlstarlet sel -N ns="http://maven.apache.org/POM/4.0.0" -t -m "/ns:project/ns:scm/ns:connection/text()" -c . -n $1 | cut -d':' -f3- | sed  's/\.git//')
+	cmd=`xml_parser`
+	echo $($cmd sel -N ns="http://maven.apache.org/POM/4.0.0" -t -m "/ns:project/ns:scm/ns:connection/text()" -c . -n "$1" | cut -d':' -f3- | sed  's/\.git//')
 }
