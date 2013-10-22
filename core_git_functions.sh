b199c18421126abbb29a33e40d29ad7b9c3fa80c
Florent Biville <florent.biville@fullsix.net>
2013-10-22 14:51:00 +0200

    Revert "Moved git treatments into methods"
    This reverts commit 00911207ec82e0c6c9810ddd72295fbfbd8b3e3a.

--- core_git_functions.sh
+++ core_git_functions.sh
@@ -12,11 +12,3 @@ jirac_get_git_current_remote_branch() {
 jirac_get_git_full_message() {
 	echo -e "$(git --git-dir="$1" log --format=%B -n1 $2)"
 }
-
-jirac_list_git_commit() {
-    echo  -e "$(git --git-dir="$1" log -10 --author="$2" --format='%h %s' $3)"
-}
-
-jirac_keep_git_commit(){
-    echo -e "$(git --git-dir="$1" log -"$2" --author="$3" --format=%h $4)"
-}
