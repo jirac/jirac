35b4b2355dbf9122520d3a3e85bbb5f4d8d3bef1
ihommani <ismaelhommani@gmail.com>
2014-07-29 23:05:49 +0200

    Moved logic from jirac script
    Removed logic from the main script inside methods.
    Also removed the poor pattern consisting in replacing
    'return' statement by 'echo' statement.

--- core_xml_functions.sh
+++ core_xml_functions.sh
@@ -10,20 +10,18 @@ xml_parser() {
 
 jirac_get_maven_version() {
 	cmd=`xml_parser`
-	echo $($cmd sel -N ns="http://maven.apache.org/POM/4.0.0" -t -m "/ns:project/ns:version/text()" -c . -n "$1")
+	project_version=$($cmd sel -N ns="http://maven.apache.org/POM/4.0.0" -t -m "/ns:project/ns:version/text()" -c . -n "$1")
 }
 
 jirac_get_maven_project_name() {
 	cmd=`xml_parser`
-	echo $($cmd sel -N ns="http://maven.apache.org/POM/4.0.0" -t -m "/ns:project/ns:name/text()" -c . -n "$1")
+	project=$($cmd sel -N ns="http://maven.apache.org/POM/4.0.0" -t -m "/ns:project/ns:name/text()" -c . -n "$1")
 }
 
-jirac_get_scm_url() {
+jirac_get_repository_url() {
 	cmd=`xml_parser`
-	echo $($cmd sel -N ns="http://maven.apache.org/POM/4.0.0" -t -m "/ns:project/ns:scm/ns:url/text()" -c . -n "$1")
-}
-
-jirac_get_connection_url() {
-	cmd=`xml_parser`
-	echo $($cmd sel -N ns="http://maven.apache.org/POM/4.0.0" -t -m "/ns:project/ns:scm/ns:connection/text()" -c . -n "$1" | cut -d':' -f3- | sed  's/\.git//')
+	repository_base_url= $($cmd sel -N ns="http://maven.apache.org/POM/4.0.0" -t -m "/ns:project/ns:scm/ns:url/text()" -c . -n "$1")
+    if [ -z "$repository_base_url" ];then
+	    repository_base_url=$($cmd sel -N ns="http://maven.apache.org/POM/4.0.0" -t -m "/ns:project/ns:scm/ns:connection/text()" -c . -n "$1" | cut -d':' -f3- | sed  's/\.git//')
+    fi
 }
