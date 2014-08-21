f699856b125a3e7959626c90d5488d46d9d0e129
Sébastien Lesaint <lesaint@ekino.com>
2014-08-21 15:41:10 +0200

    [#27] remove --print-mode option and keep only print-mode 1

--- ui_functions.sh
+++ ui_functions.sh
@@ -14,17 +14,6 @@ jirac_select() {
     done
 }
 
-jirac_apply_print_mode() {
-    case $1 in
-        "0" | "1" | 0 | 1 )
-            print_mode=$1
-            ;;
-        * )
-            message_and_exit "Print mode value is invalid"
-            ;;
-    esac
-}
-
 ##
 # put all options consistency verifications here
 ##
@@ -47,7 +36,7 @@ jirac_apply_log_level() {
 }
 
 jirac_help() {
-    echo "usage: jirac [-n number_of_commit] [ -p print_mode ] [--help] [--silent] [-grep regexp] [-l 0/1/2] [--standard-output]"
+    echo "usage: jirac [-n number_of_commit] [--help] [--silent] [-grep regexp] [-l 0/1/2] [--standard-output]"
     echo "    --number, -n : shortcut to select the n last pushed commits without interactively selecting them"
     echo "         parameter must be a positive integer"
     echo "         "
@@ -61,19 +50,6 @@ jirac_help() {
     echo "         2 : error, info and debug logs"
     echo "         "
     echo "    --grep, -g: shortcut to select commits whose short description match the provided regexp"
-    echo "         "
-    echo "    --print-mode, -p: select print_mode, supported values are "
-    echo "         0 : original printing mode of JIRAC"
-    echo "             - default print mode, used when option -p is not specified"
-    echo "             - commits are listed in most-recent-first order"
-    echo "             - commits links are displayed as items of a single first level list"
-    echo "             - commits' bodies are appended into a desrciption which is displayed as a first level item"
-    echo "             - this description can be override with an interactive prompt"
-    echo "         1 : print mode based on commit subject and body"
-    echo "             - commits are listed in oldest-first order"
-    echo "             - each commit get its own first level sublist"
-    echo "             - first level item of each commit is the subject of the commit"
-    echo "             - second level items of each commit are : the link to the commit, the body of the commit"
     echo "         "
     echo "    --standard-output: generated comment is printed to stdout instead of the clipboard"
     echo "             - useful to pipe jirac generated comment to another command"
